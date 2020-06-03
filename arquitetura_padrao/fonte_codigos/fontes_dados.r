bc807db24f95dd3d4865a9702e46cd8d3a6afd4b
CarlosChiarelli <jardins_10_2008@hotmail.com>
2020-06-03 14:08:52 -0300

    download cidades 2020

--- arquitetura_padrao/fonte_codigos/fontes_dados.r
+++ arquitetura_padrao/fonte_codigos/fontes_dados.r
@@ -47,48 +47,85 @@ baixa_historico_receitasSP_2010_2019 = function(){
 
 baixa_historico_receitasSP_2020 = function(){
 	# baixa historico mas não junta os anos
+	library(magrittr)
 
 	# criando diretorio para baixar os dados
-	caminho = '../dados/dados_receitas/2020'
+	caminho = '../dados/dados_receitas'
 	ifelse(!dir.exists(caminho), dir.create(caminho), FALSE)
 
 	# entrando nele
 	setwd(caminho)
 
-	# sequencia de anos
-	seq_anos = seq(2010, 2019)
-	url_download="https://transparencia.tce.sp.gov.br/sites/default/files/conjunto-dados/receitas-ANO.zip"    
+	cidades = readr::read_csv('../cidades_sao_paulo.csv')
 
-	for(ano in seq_anos){
+	df = cidades %>% 
+	    dplyr::mutate(
+	        nome_ok = stringr::str_replace_all(nome, ' ','-') %>% 
+	                    stringr::str_replace_all("'",'-') %>% 
+	                    abjutils::rm_accent() %>% 
+	                    stringr::str_to_lower(),
+	        url = 'https://transparencia.tce.sp.gov.br/sites/default/files/csv/receitas-cidade-2020.zip',
+	        url = stringr::str_replace(url, 'cidade', nome_ok),
+	        nome_csv = url %>% 
+	                    stringr::str_replace('https://transparencia.tce.sp.gov.br/sites/default/files/csv/','') %>% 
+	                    stringr::str_replace('.zip', '.csv')
+	    )
 
-	    print(stringr::str_c('Iniciando ANO = ', as.character(ano)))
+	primeiro = TRUE
 
-	    # variáveis auxiliares
-	    url_temp = stringr::str_replace(url_download, 'ANO', as.character(ano))
-	    nomeArquivo_zip = stringr::str_c('ano',ano,'.zip')
-	    nomeArquivo_csv = stringr::str_replace('receitas-ANO.csv', 'ANO', as.character(ano))
+	for(linha in seq(1, nrow(df))){
 
-	    # baixa
-	    print('baixando...')
-	    download.file(url_temp, nomeArquivo_zip)
-	    # dezipa
-	    unzip(nomeArquivo_zip)
-	    # remove .zip baixado
-	    file.remove(nomeArquivo_zip)
+	    nome_arq_zip = stringr::str_c(df[linha, 'nome_ok'], '.zip')
+	    url_cidade = df[linha, 'url']
+	    nome_arq_csv = df[linha, 'nome_csv']
 
-	    # desncasa 3 segundos
-	    print('descansando...')
-	    Sys.sleep(3)
+	    nome_arq_zip = as.character(nome_arq_zip)
+	    nome_arq_csv = as.character(nome_arq_csv)
+	    url_cidade = as.character(url_cidade)
+
+
+	    # excecoes de sites nao encontrados
+	    if(url_cidade != 'https://transparencia.tce.sp.gov.br/sites/default/files/csv/receitas-florinea-2020.zip' &
+	    	url_cidade != 'https://transparencia.tce.sp.gov.br/sites/default/files/csv/receitas-maraba-paulista-2020.zip' &
+	    	url_cidade != 'https://transparencia.tce.sp.gov.br/sites/default/files/csv/receitas-mococa-2020.zip' &
+	    	url_cidade != 'https://transparencia.tce.sp.gov.br/sites/default/files/csv/receitas-nantes-2020.zip' &
+	    	url_cidade != 'https://transparencia.tce.sp.gov.br/sites/default/files/csv/receitas-nova-granada-2020.zip' &
+	    	url_cidade != 'https://transparencia.tce.sp.gov.br/sites/default/files/csv/receitas-pauliceia-2020.zip' &
+	    	url_cidade != 'https://transparencia.tce.sp.gov.br/sites/default/files/csv/receitas-sao-lourenco-da-serra-2020.zip' &
+	    	url_cidade != 'https://transparencia.tce.sp.gov.br/sites/default/files/csv/receitas-sao-paulo-2020.zip'){
+	    	    
+	    	    # baixa
+	    	    print('baixando...')
+	    	    print(linha)
+	    	    download.file(url_cidade, nome_arq_zip)
+	    	    # dezipa
+	    	    unzip(nome_arq_zip)
+	    	    # remove .zip baixado
+	    	    file.remove(nome_arq_zip)
+	    
+	    	    df_aux = readr::read_csv2(nome_arq_csv, locale = readr::locale(encoding = 'latin1'))
+	    
+	    	    if(primeiro){
+	    	        df_aux %>% readr::write_csv('receita-2020.csv')
+	    	        primeiro = FALSE
+	    	    }else{
+	    	        df_aux %>% readr::write_csv('receita-2020.csv', append = TRUE)
+	    	    }
+	    
+	    	    file.remove(nome_arq_csv)
+	    }
+	    
 	}
 
 	# volto para o diretorio inicial
-	setwd('../../../fonte_codigos')
+	setwd('../../fonte_codigos')
 }
 
 
 gera_csv_receitas_10a19_Marc_Abr_Maio = function(){
 	# compila em um unico .csv
 	# meses de março, abril e maio
+	library(magrittr)
 
 	# criando diretorio para baixar os dados
 	caminho = '../dados/dados_receitas/filtrados_mes_Marc_Abr_Maio'
@@ -139,6 +176,7 @@ gera_csv_receitas_10a19_Marc_Abr_Maio = function(){
 df_cols_especif_valorSomado = function(){
 	# pegas todos anos das receitas e soma o valor agrupando pelas colunas especificadas
 	# return: dataframe consolidado
+	library(magrittr)
 
 	# indo para diretorio de dados
 	caminho = '../dados/dados_receitas/filtrados_mes_Marc_Abr_Maio'
